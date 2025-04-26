ca78fed3978a5498942fe5d9e757021dd0888b77
shamyalmahmood <shamyalmehmood@gmail.com>
2025-04-27 04:18:52 +0500

    Rough UI + OpenAI set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator'
+
+function App() {
+  return (
+    <>
+      <ImageGenerator/>
+    </>
+  )
+}
+
+export default App
